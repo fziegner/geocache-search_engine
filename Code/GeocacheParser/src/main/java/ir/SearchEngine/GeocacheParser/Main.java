6131d5108e342aa89d4d131a866c82afcd27a8f1
Fabian Ziegner <fz31fovo@studserv.uni-leipzig.de>
2018-05-29 10:33:01 +0200

    Fixed main method of parser

--- Code/GeocacheParser/src/main/java/ir/SearchEngine/GeocacheParser/Main.java
+++ Code/GeocacheParser/src/main/java/ir/SearchEngine/GeocacheParser/Main.java
@@ -1,9 +1,5 @@
 package ir.SearchEngine.GeocacheParser;
 
-import java.util.List;
-
-import org.json.JSONObject;
-
 /**
  * Main class for testing purposes
  * @author Christian Schlecht
@@ -12,10 +8,9 @@ import org.json.JSONObject;
 public class Main {
 
 	public static void main(String[] args) {
-		 String testDirectory = "C:\\Users\\Christian-PC2\\Desktop\\test";
 		
+		String testDirectory = "C:\\Users\\Christian-PC2\\Desktop\\test";
 		new Parser(testDirectory, testDirectory);
-
 	}
 
 }
