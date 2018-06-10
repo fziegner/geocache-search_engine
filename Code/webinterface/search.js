f2fef5f21b559b3aef60f27d56ba0c9740317d45
Christian Schlecht <cs85xyqa@studserv.uni-leipzig.de>
2018-06-10 10:03:19 +0200

    added js script and function to execute REST call

---
+++ Code/webinterface/search.js
@@ -0,0 +1,12 @@
+const baseURL = "http://localhost:8080/geocache-search-engine/"
+const searchURL = baseURL + "search/"
+
+function executeRESTCall(){
+    let query = document.getElementById("suche").value;
+    let xhttp = new XMLHttpRequest();
+    xhttp.open("GET", searchURL + query, false)
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
+    let response = JSON.parse(xhttp.responseText);
+    console.log(response);
+}
