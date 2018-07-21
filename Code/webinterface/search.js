9d8bee4d67933e180ae43815056315aad80765ff
Anja Fischer <af79paky@studserv.uni-leipzig.de>
2018-07-21 13:30:09 +0200

    Update search.js, optimized output for style purposes

--- Code/webinterface/search.js
+++ Code/webinterface/search.js
@@ -1,10 +1,10 @@
-const baseURL = "http://5.83.162.120:8080/geocache-search-engine/webapi/"
-const searchURL = baseURL + "search/"
+const baseURL = "http://5.83.162.120:8080/geocache-search-engine/webapi/";
+const searchURL = baseURL + "search/";
 
 function executeRESTCall(){
     let query = document.getElementById("suche").value;
     let xhttp = new XMLHttpRequest();
-    xhttp.open("GET", searchURL + query, false)
+    xhttp.open("GET", searchURL + query, false);
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send();
     let response = JSON.parse(xhttp.responseText);
@@ -23,7 +23,7 @@ function toggle_visibility(id) {
 function pprint_json_to_console(response) {
 	for(var i = 0; i < response.length; i++) {
 		console.log("array index: " + i);
-		var obj = response[i]
+		var obj = response[i];
 		for(var key in obj) {
 			var value = obj[key];
 			console.log(key +  ": " + value);
@@ -36,9 +36,12 @@ function pprint_json_to_website(response) {
 }
 
 function print_results(response) {
+	document.getElementById("content").innerHTML = "";
+	createArea("json", "content");
 	var resultset = "Results:\n\n";
 	for(var i = 0; i < response.length; i++) {
 		resultset = resultset + "ID: ".bold() + i + "\n";
+		createArea(i, "json");
 		var obj = response[i];
 		console.log("id: " + i);
 		for(var key in obj) {
@@ -47,30 +50,71 @@ function print_results(response) {
 			if(key == "name" || key == "coordinates" || key == "status" || key == "condition" || key == "hiddenAt" || key == "waypoint" || key == "cacheType" || key == "caseType" || key == "link" || key == "description") {
 				if(key == "name") {
 					resultset = resultset + "Name: ".bold() + value + "\n";
+					/*value = "Name: " + value;*/
+					createResult(key, value, i);
 				}else if (key == "coordinates") {
 					resultset = resultset + "Koordinaten: ".bold() + value + "\n";
+					value = "Koordinaten: " + value;
+					createResult(key, value, i);
 				}else if (key == "status") {
 					resultset = resultset + "Status: ".bold() + value + "\n";
+					value = "Status: " + value;
+					createResult(key, value, i);
 				}else if (key == "condition") {
 					resultset = resultset + "Zustand: ".bold() + value + "\n";
+					value = "Zustand: " + value;
+					createResult(key, value, i);
 				}else if (key == "hiddenAt") {
 					resultset = resultset + "Versteckt am: ".bold() + value + "\n";
+					value = "Versteckt am: " + value;
+					createResult(key, value, i);
 				}else if (key == "waypoint") {
 					resultset = resultset + "Wegpunkt: ".bold() + value + "\n";
+					value = "Wegpunkt: " + value;
+					createResult(key, value, i);
 				}else if (key == "cacheType") {
 					resultset = resultset + "Cacheart: ".bold() + value + "\n";
+					value = "Cacheart: " + value;
+					createResult(key, value, i);
 				}else if (key == "caseType") {
 					resultset = resultset + "Behälter: ".bold() + value + "\n";
+					value = "Behälter: " + value;
+					createResult(key, value, i);
 				}else if (key == "link") {
 					resultset = resultset + "Online: ".bold() + value.link(value) + "\n";
+					value = "Online: " + value;
+					createResult(key, value, i);
 				}else if (key == "description") {
-					resultset = resultset + "Beschreibung: ".bold() + value.substring(0,70) + "...\n";
+					resultset = resultset + "Beschreibung: ".bold() + value + "\n";
+					value = "Beschreibung: " + value;
+					createResult(key, value, i);
 				}else if (key == "logs") {
 					resultset = resultset + "Logs: ".bold() + "PLACEHOLDER" + "\n";
+					value = "Logs: " + value;
+					createResult(key, value, i);
 				}
 			}
 		}
 		resultset = resultset + "\n";
+
 	}
-	document.getElementById("json").innerHTML = resultset;
+	/*document.getElementById("json").innerHTML = resultset;*/
+}
+
+function createArea(newId, bigId) {
+    var resultArea = document.createElement("DIV");
+	resultArea.setAttribute("id", newId);
+	document.getElementById(bigId).appendChild(resultArea);
+}
+
+function createResult(newClass, result, resultNr) {
+    var resultArea = document.createElement("DIV");
+	resultArea.className = newClass;
+    var result = document.createTextNode(result);
+    resultArea.appendChild(result);
+	if (newClass == "name"){
+	document.getElementById(resultNr).prepend(resultArea);
+	} else {
+	document.getElementById(resultNr).appendChild(resultArea);
+	}
 }
