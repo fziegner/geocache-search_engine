c7513561e1846bf03e7435d4719e9020f4eeb150
Martin Potthast <martin.potthast@uni-weimar.de>
2018-10-29 15:19:37 +0100

    Added a root directory for search engine data.

--- Code/geocache-search-engine/src/main/java/ir/SearchEngine/GeocacheSearchEngine/Util/CONSTANTS.java
+++ Code/geocache-search-engine/src/main/java/ir/SearchEngine/GeocacheSearchEngine/Util/CONSTANTS.java
@@ -2,9 +2,10 @@ package ir.SearchEngine.GeocacheSearchEngine.Util;
 
 public class CONSTANTS {
 	
-	public static final String INDEX_DIRECTORY = "index";
-	public static final String DATA_DIRECTORY = "data";
-	public static final String SUGGESTER_DIRECTORY = "suggester";
+        public static final String ROOT_DIRECTORY = "/home/ubuntu/Desktop/geocache-search-tmp/";
+	public static final String INDEX_DIRECTORY = ROOT_DIRECTORY + "index";
+	public static final String DATA_DIRECTORY = ROOT_DIRECTORY + "data";
+	public static final String SUGGESTER_DIRECTORY = ROOT_DIRECTORY + "suggester";
 	public static final int MAX_SEARCH_RESULTS = 10;
     public static final int MAX_SUGGESTION_RESULTS = 5;
 }
